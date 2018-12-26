f57fb03ffdfd094b88d61aad51585ac712c019da
Maakep <schon.fredrik@gmail.com>
2018-12-26 03:00:30 +0100

    Add Custom Editor Window

---
+++ EditorExtensions/EditorWindow/CustomEditorWindow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+public class CustomEditorWindow : EditorWindow
+{
+    string text;
+
+    [MenuItem("## DGUISE ##/Example")]
+    public static void ShowWindow()
+    {
+        GetWindow<CustomEditorWindow>();
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Some text", EditorStyles.centeredGreyMiniLabel);
+        text = EditorGUILayout.TextField("Text field: ", text);
+
+        if (GUILayout.Button("Click me!"))
+        {
+            Debug.Log("You wrote: " + text);
+
+            // The Selection class gives you access to the selection
+            Debug.Log("Selected game objects: " + string.Join(",", Selection.gameObjects.Select(x => x.name).ToArray()));
+        }
+    }
+}
---
+++ EditorExtensions/EditorWindow/readme.md
@@ -0,0 +1,3 @@
+## Description
+
+This adds a menu item that, when clicked, opens up a new window where you can do your custom logic
