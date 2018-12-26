12b048510bd85387ce5cc1ff4705c111f1466695
Maakep <schon.fredrik@gmail.com>
2018-12-26 03:08:00 +0100

    Add Custom Inspector

---
+++ EditorExtensions/CustomInspector/CustomInspectorExampleComponent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CustomInspectorExampleComponent : MonoBehaviour
+{
+    public string ExampleString = "1337";
+}
+
+[CustomEditor(typeof(CustomInspectorExampleComponent))]
+public class CustomInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        CustomInspectorExampleComponent ciec = (CustomInspectorExampleComponent) target;
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Print stuff: ");
+        if (GUILayout.Button("Click me!"))
+        {
+            Debug.Log(ciec.ExampleString);
+        }
+        GUILayout.EndHorizontal();
+    }
+}
---
+++ EditorExtensions/CustomInspector/readme.md
@@ -0,0 +1,2 @@
+## Description
+This takes a MonoBehaviour script component and adds custom inspector logic
